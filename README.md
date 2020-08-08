189787e7b09ebaed7d4500892794b5b74753e050
sumukshashidhar <sumukuuu@gmail.com>
2020-08-08 07:05:56 +0530

    can now connect to mongodb

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # nodejstemplate
 A load balanced nodejs template for all use cases with authentication
+
+
+# ENV requirements
+1. PORT
+2. IP
+3. MongoURI
--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 // Imports
 var express = require("express"); 
 var mongoose = require("mongoose")
+var bodyParser = require("body-parser")
 
-// importing constants
-var ENV = require("./config/ENV_VARS.js")
+//env vars
+require('dotenv').config()
 
 
 
 // Initializations
 var app = express();
-
+app.use(express.static("styles"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 //routes file
 require('./routes')(app);
 
 
-app.listen(ENV.PORT||8080, ENV.IP || "0.0.0.0", function (req, res) {
+app.listen(process.env.PORT||8080, process.env.IP || "0.0.0.0", function (req, res) {
 	console.log("Server Started.");
 })
 
-
+mongoose.connect(process.env.mongoURI, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    useCreateIndex: true}) //Changed this line to link to a database file instead of having everything in one file to provide quick and easy access for further work
+    .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
 app.get('/', function(req, res) {
 	console.log("Hit the homepage"); 
